a912a25bedc1625213d11c5270579ed590aca895
Raja Khan <rajashahidullah@hotmail.com>
2020-07-08 12:17:53 +0200

    Commit Changes

---
+++ src/main/java/be/raja/model/Employee.java
@@ -0,0 +1,15 @@
+package be.raja.model;
+
+import java.util.Date;
+
+public class Employee {
+    private int id;
+    private String first_name;
+    private String sir_name;
+    private String telephone_number;
+    private String telephone_number_ICE;
+    private Date birth_date;
+    private double salary_per_month;
+
+
+}
